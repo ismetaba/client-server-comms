42cd7c4c86278a596bed424b0dc8566bb8431660
Revivius <Revivius@users.noreply.github.com>
2020-10-05 22:08:04 +0300

    Update Server.java
    Selam;
    
    Encapsulation;
    Bir sınıfın görevi kadar iş yapması
    Thread oluşturma ve yönetme

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -9,12 +9,16 @@ import java.util.logging.Logger;
  * @author ismet abacı
  */
 public class Server {
+    // GG:
+    // Neden private değil ?
     final static Logger logger = Logger.getLogger(String.valueOf(Server.class));
 
     /**
      * this method listens for a message on a port and process received messages
      */
     public static void main(String[] args) {
+        // GG:
+        // Port vs gibi bilgiler ayarlanabilir olmalıdır. Hardcoded olarak kullanılmamalıdır.
         int port = 49999;
         try {
             logger.info("Server started listening (port:" + port + ")...");
@@ -29,11 +33,17 @@ public class Server {
      */
     private static void runServer(int portNumber) throws IOException, InterruptedException {
         ServerSocket ss = new ServerSocket(portNumber);
+        // GG:
+        // Değişkenler neden kullanılacakları yerden uzakta tanımlanıyor ? Değişkenler
+        // mümkün mertebe kullanılacakları yere yakın tanımlanmalıdır.
         Socket s;
         BufferedReader in;
         JSONObject msg;
         String input;
         int numberOfThreads = 3,threadIndex;
+        // GG:
+        // Thread oluşturmak masraflı bir işlemdir ve oluşturulan treadlerin yönetilmesi karmaşıktır
+        // Threadleri kendimiz yaratmak dışında kullanabileceğiniz yöntem nedir ?
         MyThread [] myThreads = new MyThread[numberOfThreads];
 
         for (int i=0;i<numberOfThreads;i++){
@@ -44,8 +54,13 @@ public class Server {
         myThreads[1].setPriority(Thread.NORM_PRIORITY);
         myThreads[2].setPriority(Thread.MIN_PRIORITY);
 
+        // GG:
+        // Neden sonsuz döngü ? Bu sunucu durduralamayacak mı ?
         while (true) {
             s = ss.accept();
+            // GG:
+            // Socket bağlantısı kabul edildikten sonra gelen mesajı okumak ve önceliğine
+            // göre threadlere atamak bu sınıfın görevi mi ?
             in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             if ((input = in.readLine()) != null) {
                 msg = new JSONObject(input);
